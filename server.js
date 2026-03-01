57a764b24807e5018f7160ae86f5caaa5ab1db12
solomon344 <willamssolomon672@gmail.com>
2026-03-01 17:57:00 +0000

    added login for scrape tasks

--- server.js
+++ server.js
@@ -22,29 +22,49 @@ io.on('connection', socket => {
   console.log('🧠 WebSocket connected:', socket.id)
   
 //  connected socket instance sends a scrape:start event
-  socket.on("scrape:start",()=>{
+  socket.on("scrape:started",(data)=>{
     console.log("recieved scrape start event")
 
     // re-emit the scrape:start event to the connected socket instance
-    io.emit("scrape:start")
+    io.emit("scrape:started",{...data});
   })
 
 
   // Listen to scrape complete events
   socket.on("scrape:completed", (data) => {
-    const {scrapeId,message,status} = data
+    const {scrapeId,message,status,jobId,url} = data
     console.log("recieved scrape completed event for scrapeId:",scrapeId)
 
     // re-emit the scrape:completed event to all connected clients
-    io.emit("scrape:completed",{status,message,scrapeId});
+    io.emit("scrape:completed",{status,message,scrapeId,jobId,url});
   })
 
   // Listen to scrape failed events
   socket.on("scrape:failed", (data) => {
-    const {scrapeId,message,status} = data
+    const {scrapeId,message,status,jobId,url} = data
 
+    console.log("recieved scrape failed event for scrapeId:",scrapeId)
     // re-emit the scrape:failed event to all connected clients
-    io.emit("scrape:failed",{status, message,scrapeId});
+    io.emit("scrape:failed",{status, message,scrapeId,jobId,url});
+})
+
+// listen to scrape:fetched events
+socket.on("scrape:fetched", (data) => {
+  const {scrapeId,message,status,jobId,url} = data
+
+  console.log("recieved scrape fetched event for scrapeId:",scrapeId)
+  // re-emit the scrape:fetched event to all connected clients
+  io.emit("scrape:fetched",{status, message,scrapeId,jobId,url});
+
+})
+
+// listen to scrape:parsed events
+socket.on("scrape:parsed",(data)=>{
+  const {scrapeId,message,status,jobId,url} = data
+
+  console.log("recieved scrape parsed event for scrapeId:",scrapeId)
+  // re-emit the scrape:parsed event to all connected clients
+  io.emit("scrape:parsed",{status, message,scrapeId,jobId,url});
 })
 
 })
